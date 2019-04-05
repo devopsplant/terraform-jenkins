e64341082270369ac8f6b531462660c59d0e1058
admin <anwar.g.alawad@gmail.com>
2019-04-06 08:35:08 +1000

    Changed of design

--- module/jenkins-server.tf
+++ module/jenkins-server.tf
@@ -17,17 +17,3 @@ resource "random_string" "tracking" {
   upper   = false
 }
 
-## Creating subnets to lunch be used by the EC2 instance.
-# We want as many subnets as available
-resource "aws_subnet" "this" {
-  count             = "${length(data.aws_availability_zones.available.names)}"
-  vpc_id            = "${var.vpc_id}"
-  cidr_block        = "${var.cidr_block[count.index]}"
-  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
-
-  tags {
-    Name          = "sbn-${var.environment}-${var.region}-${var.role}-${count.index}"
-    Project       = "${var.project}"
-    Tracking      = "${upper(random_string.tracking.result)}"
-  }
-}
---
+++ module/subnets.tf
@@ -0,0 +1,14 @@
+## Creating subnets to lunch be used by the EC2 instance.
+# We want as many subnets as available
+resource "aws_subnet" "this" {
+  count             = "${length(data.aws_availability_zones.available.names)}"
+  vpc_id            = "${var.vpc_id}"
+  cidr_block        = "${var.cidr_block[count.index]}"
+  availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
+
+  tags {
+    Name          = "sbn-${var.environment}-${var.region}-${var.role}-${count.index}"
+    Project       = "${var.project}"
+    Tracking      = "${upper(random_string.tracking.result)}"
+  }
+}
