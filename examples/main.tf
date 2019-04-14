b7430745fb2259d3c2863db08389586d3c40e923
admin <anwar.g.alawad@gmail.com>
2019-04-14 12:11:35 +1000

    debug example6

--- examples/main.tf
+++ examples/main.tf
@@ -1,5 +1,5 @@
 module "jenkins" {
-  source            = "git::https://github.com/devopsplant/terraform-jenkins.git//tree/develop/module"
+  source            = "git::https://github.com/devopsplant/terraform-jenkins.git/module?ref=develop"
   project           = "jenkins"
   owner             = "devopsplant"
   provisioned_by    = "terraform"
