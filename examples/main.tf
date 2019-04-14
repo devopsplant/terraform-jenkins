d859de508e714e2a841f82c3e31e84ab0609f074
admin <anwar.g.alawad@gmail.com>
2019-04-14 11:49:30 +1000

    debug example3

--- examples/main.tf
+++ examples/main.tf
@@ -8,7 +8,7 @@ module "jenkins" {
   ssh_allowed_cidr  = ["0.0.0.0/0"]
   allowed_cidr      = ["0.0.0.0/0"]
   outbound_internet = "true"
-  vpc_id            = "vpc-31c7c755"
+  vpc_id            = "vpc-7022710000"
   cidr_block        = ["10.1.3.64/28", "10.1.3.80/28", "10.1.3.96/28"]
   environment       = "cicd"
   region            = "sydney"
