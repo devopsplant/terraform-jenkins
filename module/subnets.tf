3b7e0be25bcd1733b32399d91a85c8314cca4d1c
admin <anwar.g.alawad@gmail.com>
2019-04-14 21:37:19 +1000

    Added variables28

--- module/subnets.tf
+++ module/subnets.tf
@@ -2,12 +2,13 @@
 # We want as many subnets as available
 data "aws_vpc" "selected" {
   id = "${var.vpc_id}"
+
 }
 
 resource "aws_subnet" "this" {
   count             = "${length(data.aws_availability_zones.available.names)}"
   vpc_id            = "${data.aws_vpc.selected.id}"
-  cidr_block        = "${var.cidr_block[count.index]}"
+  cidr_block        = "${cidrsubnet(data.aws_vpc.selected.cidr_block, 4, 1)}"
   availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
 
   tags {
