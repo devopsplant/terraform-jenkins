39ed916bdbe7f0070f0e0ad91855d34d0be13087
admin <anwar.g.alawad@gmail.com>
2019-04-14 14:36:58 +1000

    Added variables10

--- module/jenkins-server.tf
+++ module/jenkins-server.tf
@@ -37,7 +37,7 @@ resource "random_string" "tracking" {
 
 resource "aws_launch_configuration" "this" {
   name_prefix          = "${var.role}-${var.environment}-${random_string.tracking.result}"
-  image_id             = "${data.aws_ami.ecs_ami.image_id}"                                 
+  image_id             = "${data.aws_ami.this.image_id}"                                 
   instance_type        = "${var.instance_type}"
   iam_instance_profile = "${aws_iam_instance_profile.this.name}"
   key_name             = "${aws_key_pair.this.key_name}"
