b71315b345ee623e356349a00c58d56f6de3033f
admin <anwar.g.alawad@gmail.com>
2019-04-14 13:57:47 +1000

    Added variables7

--- module/variables.tf
+++ module/variables.tf
@@ -13,6 +13,7 @@ variable "allowed_cidr" {
     description = "The IP ranges which will be allowed to use Jenkins from the WebInterface"
 }
 variable "role" {
+    default = "JN"
   description = "The main service of the module"
 }
 variable "vpc_id" {
