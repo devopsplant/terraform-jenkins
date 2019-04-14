b7f3c91cee5818ceb94523c810f3f3309889d76b
admin <anwar.g.alawad@gmail.com>
2019-04-14 21:25:50 +1000

    Added variables25

--- examples/main.tf
+++ examples/main.tf
@@ -1,10 +1,6 @@
 module "jenkins" {
   source            = "git::https://github.com/devopsplant/terraform-jenkins.git//module?ref=develop"
   outbound_internet = "true"
-  cidr_block        = ["10.1.3.64/28", "10.1.3.80/28", "10.1.3.96/28"]
-  environment       = "development"
-  region            = "ap-southeast-2"
-  key_name          = "jenkins"
-  ssh_pub_key       = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCyVGaw1PuEl98f4/7Kq3O9ZIvDw2OFOSXAFVqilSFNkHlefm1iMtPeqsIBp2t9cbGUf55xNDULz/bD/4BCV43yZ5lh0cUYuXALg9NI29ui7PEGReXjSpNwUD6ceN/78YOK41KAcecq+SS0bJ4b4amKZIJG3JWmDKljtv1dmSBCrTmEAQaOorxqGGBYmZS7NQumRe4lav5r6wOs8OACMANE1ejkeZsGFzJFNqvr5DuHdDL5FAudW23me3BDmrCSJ5 your_username@hostname"
+  vpc_id            = "vpc-7022710000"
   }
   
--- module/variables.tf
+++ module/variables.tf
@@ -1,5 +1,5 @@
 variable "environment" {
-  default = "Dev"
+  default = "development"
 }
 variable "region" {
   default = "ap-southeast-2"
@@ -34,10 +34,11 @@ variable "instance_type" {
 
 variable "key_name" {
   description = "the Key "
+  default = " "
 }
 variable "ssh_pub_key" {
-  
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCyVGaw1PuEl98f4/7Kq3O9ZIvDw2OFOSXAFVqilSFNkHlefm1iMtPeqsIBp2t9cbGUf55xNDULz/bD/4BCV43yZ5lh0cUYuXALg9NI29ui7PEGReXjSpNwUD6ceN/78YOK41KAcecq+SS0bJ4b4amKZIJG3JWmDKljtv1dmSBCrTmEAQaOorxqGGBYmZS7NQumRe4lav5r6wOs8OACMANE1ejkeZsGFzJFNqvr5DuHdDL5FAudW23me3BDmrCSJ5 your_username@hostname"
 }
 variable "outbound_internet" {
-  
+  default = "true"
 }
