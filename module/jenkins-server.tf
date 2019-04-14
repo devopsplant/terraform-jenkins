8f6e9c9d17c8ecba2f1a6b03878db7605c24c9f3
admin <anwar.g.alawad@gmail.com>
2019-04-14 13:54:22 +1000

    Added variables5

--- module/jenkins-server.tf
+++ module/jenkins-server.tf
@@ -15,14 +15,14 @@ data "aws_ami" "this" {
 # fatch all the availability zones and store them here
 data "aws_availability_zones" "available" {}
 #The file which have the Jenkins installation and user custom configuration in the EC2 instance
-data "template_file" "user-data-jenkins" {
-  template = "${file("${path.module}/user-data-jenkins.sh")}"
-  vars {
-    name        = "jenkins"
-    environment = "${var.environment}"
-    region      = "${var.region}"
-  }
-}
+#data "template_file" "user-data-jenkins" {
+#  template = "${file("${path.module}/user-data-jenkins.sh")}"
+#  vars {
+#    name        = "jenkins"
+#    environment = "${var.environment}"
+#    region      = "${var.region}"
+#  }
+#}
 ## EC2 Key pair
 resource "aws_key_pair" "this" {
   key_name   = "jenkins"
