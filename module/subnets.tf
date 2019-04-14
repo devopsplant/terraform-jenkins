841bd363e983bdf3c81e94c816e59949d55e165f
admin <anwar.g.alawad@gmail.com>
2019-04-14 21:46:21 +1000

    Added variables31

--- module/subnets.tf
+++ module/subnets.tf
@@ -1,14 +1,16 @@
 ## Creating subnets to lunch be used by the EC2 instance.
 # We want as many subnets as available
-data "aws_vpc" "selected" {
+data "aws_vpc" "this" {
   id = "${var.vpc_id}"
 
 }
-
+data "aws_subnet_ids" "this" {
+  vpc_id = "${var.vpc_id}"
+}
 resource "aws_subnet" "this" {
   count             = "${length(data.aws_availability_zones.available.names)}"
-  vpc_id            = "${data.aws_vpc.selected.id}"
-  cidr_block        = "${cidrsubnet(data.aws_vpc.selected.cidr_block, 4, 1)}"
+  vpc_id            = "${data.aws_vpc.this.id}"
+  cidr_block        = "${cidrsubnet(data.aws_vpc.this.cidr_block, 4, 1)}"
   availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
 
   tags {
