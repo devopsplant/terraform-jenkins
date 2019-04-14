7b12f9b9f176cc02446eb466d3d9e299426901d8
admin <anwar.g.alawad@gmail.com>
2019-04-14 23:36:13 +1000

    Added variables46

--- examples/providers.tf
+++ examples/providers.tf
@@ -2,5 +2,5 @@
 provider "aws" {
   region                  = "ap-southeast-2"
   shared_credentials_file = "~/.aws/credentials"
-  profile                 = "production"
+  profile                 = "development"
 }
