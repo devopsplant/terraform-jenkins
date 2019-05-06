a025adbb6eb6512f79fe85761f2e0d740af453e0
admin <anwar.g.alawad@gmail.com>
2019-05-06 22:54:52 +1000

    Added variables71

--- module/subnets.tf
+++ module/subnets.tf
@@ -19,7 +19,7 @@ resource "aws_subnet" "this" {
   }
 }
 resource "aws_subnet" "this1" {
-  count             = "${length(var.cidr_block) == 0 ? length(var.cidr_block) : length(data.aws_availability_zones.available.names) }"
+  count             = "${length(var.cidr_block) == 0 ? length(data.aws_availability_zones.available.names) : length(var.cidr_block) }"
   vpc_id            = "${data.aws_vpc.this.id}"
   cidr_block        = "${var.cidr_block[count.index]}"
   availability_zone = "${var.cidr_block[count.index]}"
