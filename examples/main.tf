25120101a5635c004e9f819710112fe0d8d9a0cb
admin <anwar.g.alawad@gmail.com>
2019-05-23 23:04:03 +1000

    Added variables92

--- examples/main.tf
+++ examples/main.tf
@@ -1,5 +1,5 @@
 module "jenkins" {
   source            = "git::https://github.com/devopsplant/terraform-jenkins.git//module?ref=develop"
-  cidr_block = ["10.1.3.64/28", "10.1.3.80/28", "10.1.3.96/28"]
+  cidr_block = ["10.1.3.64/28", "10.1.3.80/28", "10.1.3.96/28","10.1.3.128/28"]
   }
   
