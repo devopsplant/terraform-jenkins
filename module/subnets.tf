e0df46ecdb540dc5cf844d6ea58f99aabd6575c6
admin <anwar.g.alawad@gmail.com>
2019-05-23 23:36:03 +1000

    Added variables100

--- module/subnets.tf
+++ module/subnets.tf
@@ -10,7 +10,7 @@ data "aws_subnet_ids" "this" {
 resource "aws_subnet" "this" {
   count             = "${length(var.cidr_block) == 0 ? length(data.aws_availability_zones.available.names) : length(var.cidr_block) }"
   vpc_id            = "${data.aws_vpc.this.id}"
-  cidr_block        = "${var.cidr_block == true && length(var.cidr_block) == 0 ? cidrsubnet(data.aws_vpc.this.cidr_block, 4, count.index * 1) : var.cidr_block[count.index]}"
+  cidr_block        = "${var.cidr_block == true ? cidrsubnet(data.aws_vpc.this.cidr_block, 4, count.index * 1) : var.cidr_block[count.index]}"
 
   availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
 
