e49468fca4606c40bbde778601911319333604e6
Anwar Alawad <anwar.g.alawad@gmail.com>
2019-06-21 11:39:56 +1000

    Update user-data-jenkins.sh

--- module/user-data-jenkins.sh
+++ module/user-data-jenkins.sh
@@ -5,5 +5,9 @@ yum update -y
 
 yum install -y jenkins java-1.8.0-openjdk
 yum remove -y java-1.7.0-openjdk
+curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
+sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+sudo yum install jenkins
+sudo systemctl start jenkins
 
 reboot
